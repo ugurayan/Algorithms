2c4f7d7a703aa40f9b0a25035ae7f29f732cc22b
Ugur Ayan <ugur.ayan@gmail.com>
2017-10-13 18:25:07 +0200

    Missing Number java file

--- Java/missing-number.java
+++ Java/missing-number.java
@@ -15,30 +15,27 @@
 | Complexity : O(n)                                                               |
 | -------------------------------------------------------------------------------*/
 
-#include <stdio.h>
-#include <conio.h>
-#include <dos.h>
 
-#include<stdio.h>
-
-/********************************************
- * getMissingNumber method
- * Input #1: array
- * Input #2: size of array
- ********************************************/
-int getMissingNumber (int a[], int n)
+class Main
 {
-    int i, sum;
-    sum  = (n+1)*(n+2)/2;
-    for ( i = 0; i< n; i++)
-       sum -= a[i];
-    return sum;
-}
+    /********************************************
+     * getMissingNumber method
+     * Input #1: array
+     * Input #2: size of array
+     ********************************************/
+    static int getMissingNumber (int a[], int n)
+    {
+        int i, sum;
+        sum  = (n+1)*(n+2)/2;
+        for ( i = 0; i< n; i++)
+            sum -= a[i];
+        return sum;
+    }
 
-int main()
-{
-    int example_array[] = {4,1,7,8,3,6,5};
-    int missingNumber = getMissingNo(example_array,7);
-    printf("%d", missingNumber);
-    getchar()
+    public static void main(String args[])
+    {
+        int a[] = {4,1,7,8,3,6,5};
+        int missingNumber = getMissingNumber(a,7);
+        System.out.println(missingNumber);
+    }
 }
